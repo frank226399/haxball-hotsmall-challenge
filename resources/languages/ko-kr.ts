89f69be99b43ae8eb88566df6f5fc877ac8a8ddd
dapucita <dapucita@gmail.com>
2020-11-05 17:03:00 +0900

    typo

--- resources/languages/ko-kr.ts
+++ resources/languages/ko-kr.ts
@@ -2,6 +2,6 @@
 // THE TYPES OF PLACEHOLDER ARE LIMITED BY STRING SET.
 
 export const commontexts = {
-    advertise: '📢 haxball-hotsmall-challane\n💬 [디스코드 채팅] https://discord.gg/qfg45B2'
+    advertise: '📢 haxball-hotsmall-challange\n💬 [디스코드 채팅] https://discord.gg/qfg45B2'
     ,shutdown: '📢 방이 곧 닫힙니다. 이용해주셔서 감사합니다.'
 }
