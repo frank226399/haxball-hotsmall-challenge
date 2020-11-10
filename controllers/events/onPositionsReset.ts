f8470dedc7aa2d31df07530853a4bfce4e4208e2
dapucita <dapucita@gmail.com>
2020-11-10 23:08:38 +0900

    bug fix

--- controllers/events/onPositionsReset.ts
+++ controllers/events/onPositionsReset.ts
@@ -1,6 +1,7 @@
 import {  } from "../../resources/lang";
 
 export function onPositionsResetListener(): void {
+    window.afkDetector.tickCounter = 0; // reset counter init
     switch(window.afkDetector.teamPicker) {
         case 1: { // turn for red kicks the ball
             window.afkDetector.redActivity = false;
